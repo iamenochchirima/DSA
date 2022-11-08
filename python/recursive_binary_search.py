7fab8519afaa635e8b434d2b0585baebf62ed575
iamenochchirima <iamenochchirima@gmail.com>
2022-11-08 08:50:08 +0200

    recursion commit

---
+++ python/recursive_binary_search.py
@@ -0,0 +1,23 @@
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = len(list)//2
+
+    if list[midpoint] == target:
+        return True
+    else:
+        if list[midpoint] < target:
+            return recursive_binary_search(list[midpoint+1:], target)
+        else:
+            return recursive_binary_search(list[:midpoint], target)
+
+def verify(result):
+    print("Target found", result)
+
+numbers  = range(1, 11)
+result = recursive_binary_search(numbers, 12)
+verify(result)
+
+result = recursive_binary_search(numbers, 6)
+verify(result)
