f3c73542b5ac20f0f1ef89a7a4f4c0e2a82a296b
iamenochchirima <iamenochchirima@gmail.com>
2022-11-09 17:22:36 +0200

    linked list merge sort

--- python/linked_list_remove.py
+++ python/linked_list_remove.py
@@ -84,7 +84,7 @@ class LinkedList:
             current = self.head
 
             while position > 1:
-                current = node.next_node
+                current = current.next_node
                 position -= 1
 
             prev_node = current
@@ -116,6 +116,19 @@ class LinkedList:
 
                 previous = current
                 current = current.next_node
+        return current
+
+    def node_at_index(self, index):
+        if index == 0:
+            return self.head
+        else:
+            current = self.head
+            position = 0
+        
+        while position < index:
+            current = current.next_node
+            position += 1
+        
         return current
 
 
