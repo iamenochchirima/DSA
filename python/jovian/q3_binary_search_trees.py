3ca3d5745cbec73160138fb45b70d9debc1aaa24
iamenochchirima <iamenochchirima@gmail.com>
2022-11-15 18:57:59 +0200

    q3

---
+++ python/jovian/q3_binary_search_trees.py
@@ -0,0 +1,82 @@
+"""
+QUESTION : As a senior backend engineer at Jovian, you are tasked with developing a fast in-memory data structure to manage profile information (username, name and email) for 100 million users. It should allow the following operations to be performed efficiently:
+
+Insert the profile information for a new user.
+Find the profile information of a user, given their username
+Update the profile information of a user, given their usrname
+List all the users of the platform, sorted by username
+You can assume that usernames are unique.
+
+Problem: We need to create datastructure which can store 100 million records and perform insertion,
+ search, update and list operations efficiently.
+
+ Input examles:
+
+aakash = User('aakash', 'Aakash Rai', 'aakash@example.com')
+biraj = User('biraj', 'Biraj Das', 'biraj@example.com')
+hemanth = User('hemanth', 'Hemanth Jain', 'hemanth@example.com')
+jadhesh = User('jadhesh', 'Jadhesh Verma', 'jadhesh@example.com')
+siddhant = User('siddhant', 'Siddhant Sinha', 'siddhant@example.com')
+sonaksh = User('sonaksh', 'Sonaksh Kumar', 'sonaksh@example.com')
+vishal = User('vishal', 'Vishal Goel', 'vishal@example.com')
+"""
+
+class User:
+    
+    def __init__(self, username, name, email):
+        self.username = username
+        self.name = name
+        self.email = email
+        
+
+    def __repr__(self):
+        return "User(username='{}', name='{}', email='{}')".format(self.username, self.name, self.email)
+    
+    def __str__(self):
+        return self.__repr__()
+
+class UserDatabase:
+
+    def __init__(self):
+        self.users = []
+
+    def insert(self, user):
+        i = 0
+        while i < len(self.users):
+            if self.users[i].username > user.username:
+                break
+            i += 1
+        self.users.insert(i, user)
+    
+    def find(self, username):
+        for user in self.users:
+            if user.username == username:
+                return user
+    
+    def update(self, user):
+        target = self.find(user.username)
+        target.name, target.email = user.name, user.email
+        
+    def list_all(self):
+        return self.users
+
+aakash = User('aakash', 'Aakash Rai', 'aakash@example.com')
+biraj = User('biraj', 'Biraj Das', 'biraj@example.com')
+hemanth = User('hemanth', 'Hemanth Jain', 'hemanth@example.com')
+enoch = User('enoch', 'Enoch Chirima', 'enoch@gmaiil.com')
+siddhant = User('siddhant', 'Siddhant Sinha', 'siddhant@example.com')
+sonaksh = User('sonaksh', 'Sonaksh Kumar', 'sonaksh@example.com')
+vishal = User('vishal', 'Vishal Goel', 'vishal@example.com')
+
+database = UserDatabase()
+
+database.insert(enoch)
+database.insert(siddhant)
+database.insert(vishal)
+database.insert(biraj)
+
+users = database.list_all()
+
+print(users)
+
+
