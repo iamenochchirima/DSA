32dade3087cc84c41c7b39a64129197534148f8b
iamenochchirima <iamenochchirima@gmail.com>
2022-11-18 08:58:04 +0200

    tuple to binary and vice versa

--- python/jovian/q4_bst_tree_to_tuple_and_vice_vesa.py
+++ python/jovian/q4_bst_tree_to_tuple_and_vice_vesa.py
@@ -40,4 +40,26 @@ def parse_tuple(data):
         node = TreeNode(data)
     return node
 
-tree2 = parse_tuple(((1,3,None),2,((None,3,4),5,(6,7,8))))
+tree2 = parse_tuple(tree_tuple)
+
+
+"""
+QUESTION: Define a function tree_to_tuple that converts a binary tree into a tuple representing the 
+same tree. E.g. tree_to_tuple converts the tree created above to the tuple ((1, 3, None), 2, ((None, 
+3, 4), 5, (6, 7, 8))). Hint: Use recursion.
+"""
+
+def tree_to_tuple(node):
+    if isinstance(node, TreeNode):
+        if node.left is None and node.right is None:
+            return node.key
+        return (
+            tree_to_tuple(node.left),
+            node.key,
+            tree_to_tuple(node.right)
+        )
+    else:
+        return node
+
+
+print(tree_to_tuple(tree2))
