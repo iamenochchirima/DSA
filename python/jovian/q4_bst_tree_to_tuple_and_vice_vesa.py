ad5a89d86ad134e470c0b107256a4441bf572f80
iamenochchirima <iamenochchirima@gmail.com>
2022-11-21 22:04:30 +0200

    Finished

--- python/jovian/q4_bst_tree_to_tuple_and_vice_vesa.py
+++ python/jovian/q4_bst_tree_to_tuple_and_vice_vesa.py
@@ -355,6 +355,57 @@ def make_balanced_bst(data, lo=0, hi=None, parent=None):
     
     return root
     
+"""
+Balancing an Unbalanced BST
+QUESTION: Write a function to balance an unbalanced binary search tree.  
+"""
+
+def balance_bst(node):
+    return make_balanced_bst(list_all(node))
+
+tree1 = None
+
+for user in users:
+    tree1 = insert(tree1, user.username, user)
+
+"""
+A Python-Friendly Treemap
+We are now ready to return to our original problem statement.
+
+QUESTION 1: As a senior backend engineer at Jovian, you are tasked with developing a fast in-memory data structure to manage profile information (username, name and email) for 100 million users. It should allow the following operations to be performed efficiently:
+
+Insert the profile information for a new user.
+Find the profile information of a user, given their username
+Update the profile information of a user, given their usrname
+List all the users of the platform, sorted by username
+You can assume that usernames are unique.
+"""
+
+class TreeMap():
+    def __init__(self):
+        self.root = None
+        
+    def __setitem__(self, key, value):
+        node = find(self.root, key)
+        if not node:
+            self.root = insert(self.root, key, value)
+            self.root = balance_bst(self.root)
+        else:
+            update(self.root, key, value)
+            
+        
+    def __getitem__(self, key):
+        node = find(self.root, key)
+        return node.value if node else None
+    
+    def __iter__(self):
+        return (x for x in list_all(self.root))
+    
+    def __len__(self):
+        return tree_size(self.root)
+    
+    def display(self):
+        return display_keys(self.root)
 
 
 
