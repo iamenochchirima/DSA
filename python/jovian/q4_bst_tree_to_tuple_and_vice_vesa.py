3a4417a5afb8b275413550289a511b44f8cccc61
iamenochchirima <iamenochchirima@gmail.com>
2022-11-21 17:19:22 +0200

    more

--- python/jovian/q4_bst_tree_to_tuple_and_vice_vesa.py
+++ python/jovian/q4_bst_tree_to_tuple_and_vice_vesa.py
@@ -258,4 +258,104 @@ tree.left.parent = tree
 tree.right = BSTNode(sonaksh.username, sonaksh)
 tree.right.parent = tree
 
-print(display_keys(tree))
+def insert(node, key, value):
+    if node is None:
+        node = BSTNode(key, value)
+    elif key < node.key:
+        node.left = insert(node.left, key, value)
+        node.left.parent = node
+    elif key > node.key:
+        node.right = insert(node.right, key, value)
+        node.right.parent = node
+    return node
+
+insert(tree, biraj.username, biraj)
+insert(tree, sonaksh.username, sonaksh)
+insert(tree, aakash.username, aakash)
+insert(tree, hemanth.username, hemanth)
+insert(tree, siddhant.username, siddhant)
+insert(tree, vishal.username, siddhant)
+
+"""  
+Finding a Node in BST
+QUESTION: Find the value associated with a given key in a BST.
+"""
+
+def find(node, key):
+    if node is None:
+        return None
+    if key == node.key:
+        return node
+    if key < node.key:
+        return find(node.left, key)
+    if key > node.key:
+        return find(node.right, key)
+
+
+
+node = find(tree, 'enoch')
+
+""" 
+Updating a value in a BST
+QUESTION: Write a function to update the value associated with a given key within a BST
+"""
+
+def update(node, key, value):
+    target = find(node, key)
+    if target is not None:
+        target.value = value
+
+update(tree, 'enoch', User('enock', 'Enock Ngoni', 'enochn@example.com'))
+
+node = find(tree, 'enock')
+
+"""
+List the nodes
+QUESTION 13: Write a function to retrieve all the key-values pairs stored in a BST in the sorted 
+order of keys.  
+"""
+
+def list_all(node):
+    if node is None:
+        return []
+    return list_all(node.left) + [(node.key, node.value)] + list_all(node.right)
+
+"""
+Balanced Binary Trees
+QUESTION: Write a function to determine if a binary tree is balanced.  
+"""
+
+def is_balanced(node):
+    if node is None:
+        return True, 0
+    balanced_l, height_l = is_balanced(node.left)
+    balanced_r, height_r = is_balanced(node.right)
+    balanced = balanced_l and balanced_r and abs(height_l - height_r) <=1
+    height = 1 + max(height_l, height_r)
+    return balanced, height
+
+"""
+Balanced Binary Search Trees
+QUESTION: Write a function to create a balanced BST from a sorted list/array of key-value pairs.  
+"""
+
+def make_balanced_bst(data, lo=0, hi=None, parent=None):
+    if hi is None:
+        hi = len(data) - 1
+    if lo > hi:
+        return None
+    
+    mid = (lo + hi) // 2
+    key, value = data[mid]
+
+    root = BSTNode(key, value)
+    root.parent = parent
+    root.left = make_balanced_bst(data, lo, mid-1, root)
+    root.right = make_balanced_bst(data, mid+1, hi, root)
+    
+    return root
+    
+
+
+
+
